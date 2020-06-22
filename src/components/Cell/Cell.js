ccdefaacbe4c1dd11adc02da12dbba497d9e5bc3
paulo <paulo.felicio@gmail.com>
2020-06-22 22:14:40 +0200

    Optimize Cell rendetring
    Avoid rendering the component in case props do not change.

--- src/components/Cell/Cell.js
+++ src/components/Cell/Cell.js
@@ -7,11 +7,21 @@ class Cell extends Component {
       super(props);
    }
 
+   shouldComponentUpdate(nextProps) {
+      const diffs = Object.keys(this.props).map(
+         (prop) => nextProps[prop] === this.props[prop]
+      );
+
+      return diffs.indexOf(false) >= 0 ? true : false;
+   }
+
    render() {
       const className = this.props.alive ? classes.CellAlive : classes.CellDead;
       const cellStyle = {
          top: `${this.props.y * this.props.cellSize}em`,
          left: `${this.props.x * this.props.cellSize}em`,
+         width: `${this.props.cellSize}em`,
+         height: `${this.props.cellSize}em`,
       };
 
       return <div style={cellStyle} className={className}></div>;
